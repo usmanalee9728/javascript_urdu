becc21bf7194e809afcab98f8043ad53fc748aad
usmanalee9728 <usmanalee9728@gmail.com>
2025-08-29 06:29:35 +0000

    learn new topic variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 9728603
+let accountEmail = "usmanali@google.com"
+var accountPassword = "454545"
+accountCity = 'Mansehra'
+let account_state;
+
+// accountId = 2233
+
+
+accountEmail = "ali@gmail.com"
+accountPassword = "5656565656"
+accountCity = 'abbottabad'
+account_state = 'kpk'
+
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, account_state]);
