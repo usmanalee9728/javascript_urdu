081aae80481b0b63a3fcd30aa5a7aac5beff6a9c
usmanalee9728 <usmanalee9728@gmail.com>
2025-08-30 07:38:08 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3 + 3 ) we are using node js not browser
+
+console.log(3 + 3) // code readability should be high
+
+console.log("usman")
+
+
+
+let name = "usman"
+let age = 22
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint 
+// string =""
+// boolean => true/ false
+// null => stand alone value
+// undefined => undefined
+// symbol => use for uniquness
+
+
+
+//object
+
+console.log(typeof undefined);
+console.log(typeof null);
+
+
