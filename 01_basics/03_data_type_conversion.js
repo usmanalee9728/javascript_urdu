377991aba8f130a10bfab3e9b2292ee31c9a5132
usmanalee9728 <usmanalee9728@gmail.com>
2025-08-31 07:08:08 +0000

    datatypes conversion

---
+++ 01_basics/03_data_type_conversion.js
@@ -0,0 +1,43 @@
+let score = "44"
+
+//  console.log(typeof score);
+//  console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+
+// "33" => 33
+//  "33ac"  => Nan
+// true => 1; false => 0;
+
+
+let isloggedin = "usman"
+
+let booleanisloggedin = Boolean(isloggedin)
+//  console.log(booleanisloggedin);
+ 
+
+//  1 => true; 0 => false;
+//  "" => false; "usman" => true;
+  let somenumber = 55
+
+  let stringNumber = String(somenumber)
+
+  console.log(typeof stringNumber);
+  console.log(stringNumber);
+
+
+//   ********************************************  operations ***********************************
+
+
+
+  
+  
+
+ 
+ 
